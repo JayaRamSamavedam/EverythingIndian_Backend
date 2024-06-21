37e421f22b316db923a45c5e262b554fd2b6e157
JayaRamSamavedam <98570026+JayaRamSamavedam@users.noreply.github.com>
2024-06-21 12:48:56 +0530

    new commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -133,19 +133,19 @@ export const userLogin = async (req, res) => {
 
         // secure : true
         // httpOnly: true,
-        res.cookie('jwt', refreshToken, {
-          // sameSite: 'Lax', 
-          sameSite: 'None',
-          secure: true, // Secure flag must be true when sameSite is 'None'
-          maxAge: 24 * 60 * 60 * 1000,
-          httpOnly: true, // Recommended for security reasons
-          domain: 'reimagined-fishstick-sigma.vercel.app',
-          path: '/' // Ensure this matches your application route structure
-      });
-    //   res.setHeader('Set-Cookie', [
-    //     `jwt=${refreshToken}`,
-    //     // 'type=ninja; Path=/'
-    // ]);
+      //   res.cookie('jwt', refreshToken, {
+      //     // sameSite: 'Lax', 
+      //     sameSite: 'None',
+      //     secure: true, // Secure flag must be true when sameSite is 'None'
+      //     maxAge: 24 * 60 * 60 * 1000,
+      //     httpOnly: true, // Recommended for security reasons
+      //     domain: 'reimagined-fishstick-sigma.vercel.app',
+      //     path: '/' // Ensure this matches your application route structure
+      // });
+      res.setHeader('Set-Cookie', [
+        `jwt=${refreshToken}`,
+        // 'type=ninja; Path=/'
+    ]);
     res.setHeader("Access-Control-Allow-Credentials","true");
 res.status(200).json({ message: "User login successfully done", accessToken:accessToken });
       } else {
